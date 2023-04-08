8eeb740a2ec904711b52068e1b36cb18dd6afe1b
Renal <rnlaminev@gmail.com>
2023-04-08 20:08:06 +0300

    Example005

---
+++ Example/Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "миляуша")
+{
+    Console.WriteLine("Привет, Моя дорогая");
+}
+else 
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
